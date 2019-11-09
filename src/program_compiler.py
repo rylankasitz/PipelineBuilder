6a0b161460af34e678253f126d48a6b6fb46bbb7
Rylan Kasitz <rylankasitz@ksu.edu>
2019-11-08 23:15:22 -0600

    reorginized

---
+++ src/program_compiler.py
@@ -0,0 +1,6 @@
+import utils
+import configs.program_compiler as program_compiler
+
+program = program_compiler.ProgramBlock()
+
+#utils.create_shell_file()
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,16 @@
 def create_shell_file(location, shell_args):
-    f = open(location, "a")
+    f = open(location, "w")
     f.write("#!/bin/bash")
     for arg in shell_args:
         f.write("#SBATCH" + arg)
+    f.close()
+
+    return location
+
+def write_shell_args(location, cmd_args):
+    f = open(location, "a")
+    f.write("while [ "$1" != "" ]; do\n\tcase $1 in\n")
+    for arg in cmd_args:
+        f.write("\t\t" + arg + ")\t\tshift\n\t\t\t\t" + arg  + "=$1\n\t\t\t\t;;")
+    f.write("\tesac\n\tshift\ndone")
     f.close()
