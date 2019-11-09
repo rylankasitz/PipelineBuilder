e3ba1546c863125f9c9e8a6f514b414edad6f351
zachwood0s <zach@hayzak.com>
2019-11-09 12:34:41 -0600

    started working on pipeline compiler

--- src/pipeline_compiler.py
+++ src/pipeline_compiler.py
@@ -12,6 +12,48 @@ def add_excecution_order(blocks, outputs, run_num):
         blocks[output.input_uuid].run_order = max(blocks[output.input_uuid].run_order, run_num + 1)
         add_excecution_order(blocks, blocks[output.input_uuid].outputs, run_num + 1)
 
+class Env:
+    def __init__(self):
+        self.directory = ""
+        self.program_location = ""
+
+def compile_(block):
+    return {
+        "pipeline": compile_pipeline,
+        "program": compile_program,
+        "loop": compile_loop,
+        "constant": compile_constant,
+    }[block.type](block)
+
+def compile_pipeline(pipe):
+    utils.create_shell_file("name_of_some_sort")
+    utils.write_shell_args("do_args")
+    for b in pipe.blocks:
+        compile_(b)
+    # ending stuff
+
+def compile_program(block, env):
+    program_block = utils.get_block(block.name)
+
+    def gen_outputs():
+        for out in block.outputs:
+            output_type = program_block.output_types[out.output_name]
+            arg = "--{} {}/../{}{}".format(
+                out.output_name,
+                env.directory,
+                block.name,
+                output_type
+            )
+            yield arg
+
+    args = " ".join(gen_outputs())
+    line = "sbatch {}/{}.sh {} {}".format(env.program_location, block.name, "",args)
+
+def compile_loop():
+    pass
+
+def compile_constant():
+    pass
 pipeline = utils.get_pipeline("test")
 
 add_excecution_order(pipeline.blocks, pipeline.outputs, 0)
