80646cec32aa86e987fb78dccb27e72782a23bdd
Rylan Kasitz <rylankasitz@ksu.edu>
2019-11-09 13:10:14 -0600

    added program

---
+++ programs/run_add_count.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH--time=1:00
+
+export PATH="/../test_programs/:$PATH"
+
+while [ "$1" != "" ]; do
+	case $1 in
+		--input) shift
+					input=$1
+					;;
+		--count) shift
+					count=$1
+					;;
+		--output) shift
+					output=$1
+					;;
+	esac
+	shift
+done
+
+sbatch $repeat --input $input --count $count --output $output
